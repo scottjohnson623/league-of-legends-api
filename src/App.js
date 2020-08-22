bc9ebdb971dae32d34c752ef15cbd03c775a22a5
SS Johnson <scott.johnson623@gmail.com>
2020-08-22 11:33:50 +0900

    initializing react-redux

--- src/App.js
+++ src/App.js
@@ -1,73 +1,11 @@
 import React, { useState } from "react";
-import "./App.css";
-import axios from "axios";
-import championData from "./data/champion.json";
-
+import ".styles/App.css";
+import SearchField from "./components/SearchField";
 function App() {
-  let [summonerName, setSummonerName] = useState("");
-  let [accountID, setAccountID] = useState("");
-  let [championID, setChampionID] = useState();
-  let [matches, setMatches] = useState([]);
-  let [view, setView] = useState("main");
-
-  function makechampionSelectBar() {
-    let result = [];
-    for (const key in championData.data) {
-      result.push(
-        <option key={key} value={championData.data[key].key}>
-          {key}
-        </option>
-      );
-    }
-    return result;
-  }
-
-  async function getData() {
-    try {
-      let match = await axios.get(`/${summonerName}/${championID}`);
-      setMatches(match.data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  function postMatchData() {
-    console.log("matches.data", matches.data);
-    return matches.map((elem) => {
-      return <p>{elem.gameId}</p>;
-    });
-  }
   return (
-    <div className="App">
-      Summoner Name:{" "}
-      <input
-        type="text"
-        onChange={(e) => {
-          setSummonerName(e.target.value);
-        }}
-      ></input>{" "}
-      {summonerName}
-      <br />
-      Champion:
-      <select
-        name="Champion"
-        label="champion"
-        onChange={(e) => {
-          setChampionID(e.target.value);
-        }}
-      >
-        {makechampionSelectBar()}
-      </select>
-      {championID}
-      <br />
-      <button
-        onClick={() => {
-          getData();
-        }}
-      >
-        SEND IT
-      </button>
-      {postMatchData()}
-    </div>
+    <>
+      <SearchField />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import dotenv from "dotenv";
-dotenv.config();
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import store from "./reducers/index";
+import { Provider } from "react-redux";
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
